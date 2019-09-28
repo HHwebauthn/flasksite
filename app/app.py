6fa36156a4962d0b712206b595105d6b8ecc7362
Hunter Hayward <Hunter@webauthn.works>
2019-09-28 12:01:08 -0600

    added URL page

--- app/app.py
+++ app/app.py
@@ -5,3 +5,9 @@ from flask import Flask, render_template, url_for
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/add')
+def add():
+	return render_template('add.html')
+
+	
