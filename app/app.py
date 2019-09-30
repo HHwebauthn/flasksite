cbd0e4e4c1980c40e73f352218cdc4c1368e9b0c
Hunter Hayward <Hunter@webauthn.works>
2019-09-29 19:10:26 -0600

    validates url in add url

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from app import app
 from flask import Flask, render_template, url_for, request, redirect, flash
 from datetime import datetime
 from logging import DEBUG
+from forms import BookmarkForm
 
 bookmarks = []
 app.config['SECRET_KEY'] = b'I\x97r\x9e\xd2\xe5\xf6\xd7\x07\x84B\x17D\x04^\xd1\x17O\xd2\xb2cI2\xaa'
@@ -22,12 +23,14 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-	if request.method == "POST":
-		url = request.form['url']
-		store_bookmark(url)
-		flash("Stored bookmark '{}'".format(url))
+	form = BookmarkForm()
+	if form.validate_on_submit():
+		url = form.url.data
+		description = form.description.data
+		store_bookmark(url, description)
+		flash("Stored '{}'".format(description))
 		return redirect(url_for('index'))
-	return render_template('add.html')
+	return render_template('add.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
